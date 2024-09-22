cf97673f8704c0c005b7816417c51327ddd975c6
mana147 <phamhieu078@gmail.com>
2024-09-22 22:13:01 +0700

    update

--- get_ip.sh
+++ get_ip.sh
@@ -4,30 +4,30 @@
 ip=$(curl -s ipinfo.io/ip)
 
 # Ghi thông tin IP vào file ip.log, kèm dấu xuống dòng
-echo $ip >> /home/nevermore/Project/ip/log.txt
+echo $ip >> /root/ip/log.txt
 
-cd /home/nevermore/Project/ip
+cd /root/ip
 # Thực hiện các lệnh Git
 # Thực hiện git add -A và kiểm tra xem lệnh có thành công không
 if git --git-dir=.git add -A; then
-    echo "Lệnh 'git add' đã được thực thi thành công." >> /home/nevermore/Project/ip/log.txt
+    echo "Lệnh 'git add' đã được thực thi thành công." >> /root/ip/log.txt
 else
-    echo "Lỗi khi thực thi lệnh 'git add'." >> /home/nevermore/Project/ip/log.txt
-    exit 1 >> /home/nevermore/Project/ip/log.txt
+    echo "Lỗi khi thực thi lệnh 'git add'." >> /root/ip/log.txt
+    exit 1 >> /root/ip/log.txt
 fi
 
 # Thực hiện git commit và kiểm tra xem lệnh có thành công không
-if git --git-dir=/home/nevermore/Project/ip/.git commit -m "update"; then
-    echo "Lệnh 'git commit' đã được thực thi thành công." >> /home/nevermore/Project/ip/log.txt
+if git --git-dir=.git commit -m "update"; then
+    echo "Lệnh 'git commit' đã được thực thi thành công." >> /root/log.txt
 else
-    echo "Lỗi khi thực thi lệnh 'git commit'." >> /home/nevermore/Project/ip/log.txt
-    exit 1 >> /home/nevermore/Project/ip/log.txt
+    echo "Lỗi khi thực thi lệnh 'git commit'." >> /root/log.txt
+    exit 1 >> /root/ip/log.txt
 fi
 
 # Thực hiện git push và kiểm tra xem lệnh có thành công không
 if git --git-dir=.git push; then
-    echo "Lệnh 'git push' đã được thực thi thành công." >> /home/nevermore/Project/ip/log.txt
+    echo "Lệnh 'git push' đã được thực thi thành công." >> /root/ip/log.txt
 else
-    echo "Lỗi khi thực thi lệnh 'git push'." >> /home/nevermore/Project/ip/log.txt
-    exit 1 >> /home/nevermore/Project/ip/log.txt
+    echo "Lỗi khi thực thi lệnh 'git push'." >> /root/ip/log.txt
+    exit 1 >> /root/ip/log.txt
 fi
